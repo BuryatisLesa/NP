b58e71fc81da34bca68b114b56e72043232edec6
unknown <kostyansandanov@gmail.com>
2024-08-21 20:56:06 +0800

    Добавлен PostList для вывода данных из модели Post

--- newsportal/views.py
+++ newsportal/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    """ Вывод постов"""
+    model = Post
+    ordering = 'title'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        contex['time_now'] = datetime.utcnow()
+        contex['next_posts'] = "Завтра будут новости!"
+        return contex
+
+
+class PostDetail(DetailView):
+    """Вывод отдельных постов"""
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
